5334b0a0829ef80cda93063dc93baa518d67a98e
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2014-03-25 20:20:25 -0400

    Refactor functions into files.

--- util.go
+++ util.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"os"
+	"regexp"
 )
 
 func fileExists(name string) bool {
@@ -11,4 +13,24 @@ func fileExists(name string) bool {
 		}
 	}
 	return true
+}
+
+// postExists determines whether or not a particular post is already present
+// by checking the filename it should occupy.
+func postExists(location, slug string) bool {
+	return fileExists(location + "/source/_posts/" + slug + ".md")
+}
+
+func postSlugFromFilename(filename string) (string, error) {
+	re, err := regexp.Compile(`source/_posts/(.+)\.md`)
+	if err != nil {
+		return "", err
+	}
+	res := re.FindStringSubmatch(filename)
+
+	if len(res) < 2 {
+		return "", errors.New("Unable to resolve post slug")
+	}
+
+	return res[1], nil
 }
